c2d0646d769c441d9935e18bab44cfac30d138d5
Kipkerich <jkipkerich@kabarak.ac.ke>
2024-04-10 08:58:11 +0300

    User input function

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Input name
+
+name = input('what is your name ?')
+
+# Input age
+age = input('How old are you?')
+
+#Input Location
+location = input('Where do you reside?')
+
+print("Hello", name,"You are", age , "years old living in", location)
