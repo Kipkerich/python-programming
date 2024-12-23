b5fd1536e8ae928d199e20f3811f9ef9689ccba4
Kipkerich <jkipkerich@kabarak.ac.ke>
2024-12-24 00:23:44 +0300

    Week 4 assignment

---
+++ week4_assignment.py
@@ -0,0 +1,12 @@
+#creating a new file
+file =open ("new.txt", 'w')
+
+#writing on the file
+file.write("Welcome to this page, I am Jacob Korir")
+
+#reading the file
+
+file = open ("new.txt", 'r')
+
+print(file.read())
+
